18b7b3c2a48806041d21fbc7b424434f1f678277
jayd308 <jeandamour157@gmail.com>
2025-04-14 13:22:58 +0200

    this is my website

---
+++ script.js
@@ -0,0 +1,48 @@
+let menuIcon =document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
+
+let sections=document.querySelectorAll('section');
+let navlinks=document.querySelectorAll('header nav a')
+window.onscroll = ()=>{
+    sections.forEach(sec=>{
+        let top=window.scrollY
+        let offset = sec.offsetTop -150;
+        let height =sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >=offset && top < offset + height){
+            navlinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            })
+        }
+    })
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY>100)
+
+    menuIcon.classList.remove('bx-x')
+    navbar.classList.remove('active')
+}
+
+scrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+})
+ScrolpReveal().reveal('.home-content, .heading',{origin: 'top'})
+ScrolpReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form',{origin: 'bottom'})
+ScrolpReveal().reveal('.home-content h1, .about-img',{origin: 'left'})
+ScrolpReveal().reveal('.home-content p, .about-content',{origin: 'right'})
+
+const type= new typed('.multiple-text',{
+    strings: ['Fronted Developer','youtube','Blogger'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
